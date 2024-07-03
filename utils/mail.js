b861e951e2f17b5c57225e2f70234f68fe043442
GDD-impact <gddimpactsoftwares@gmail.com>
2024-07-03 14:41:33 +0100

    updates

--- utils/mail.js
+++ utils/mail.js
@@ -8,7 +8,7 @@ export const sendVerificationEmail = async (email, token) => {
 
   try {
     await resend.emails.send({
-      from: "ICCapp <emails@innovationscybercafe.com>", // Set your "from" email address
+      from: "ICCapp <onboarding@resend.dev>", // Set your "from" email address
       to: email,
       subject: "Verify your email",
       html: `<p>Click <a href="${confirmLink}">here</a> to verify your email.</p>`,
