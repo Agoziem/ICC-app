2b17fd5090dbcc5ba9e88459b052280a4ea369ec
GDD-impact <gddimpactsoftwares@gmail.com>
2024-10-11 06:12:13 +0100

    update

--- components/Whatsapp/ChatMessage.jsx
+++ components/Whatsapp/ChatMessage.jsx
@@ -16,7 +16,7 @@ const ChatMessage = ({ message }) => {
     data: mediaUrl,
     isLoading,
     error,
-  } = useSWR(message.media_id && null, getMedia);
+  } = useSWR(message?.media_id || null, getMedia);
 
   const messageTime = new Date(message.timestamp);
 
@@ -47,111 +47,107 @@ const ChatMessage = ({ message }) => {
           color: message.message_mode === "sent" ? "white" : "var(--primary)",
         }}
       >
-        {
-          // Display the message body if the message type is text
-          message.message_type === "text" ? message.body : null
-        }
+        {/* // Display the message body if the message type is text */}
+        {message.message_type === "text" ? message.body : null}
 
-        {
-          // Show a loading spinner while the media is loading
-          isLoading && (
-            <div className="d-flex justify-content-center mt-2">
-              <div className="spinner-border text-primary" role="status">
-                <span className="visually-hidden">Loading...</span>
+        {/* // Show a loading spinner while the media is loading */}
+        {!error && isLoading && (
+          <div className="d-flex justify-content-center mt-2">
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )}
+
+        {/* // Show a loading error if there is any error */}
+        {error && !isLoading && (
+          <div className="d-flex justify-content-center mt-2">
+            <p className="text-primary">
+              an error occured while fetching media
+            </p>
+          </div>
+        )}
+
+        {/* // Display the media if the message type is media */}
+        {message.message_type === "image" && mediaUrl && (
+          <div className="mt-2">
+            <img
+              src={mediaUrl}
+              alt="media"
+              style={{ maxWidth: "70%", borderRadius: "10px" }}
+            />
+            {message?.caption}
+          </div>
+        )}
+
+        {/* // Display the media if the message type is media */}
+        {message.message_type === "sticker" && mediaUrl && (
+          <div className="mt-2">
+            <img
+              src={mediaUrl}
+              alt="media"
+              style={{ maxWidth: "70%", borderRadius: "10px" }}
+            />
+          </div>
+        )}
+
+        {/* // Display the media if the message type is media */}
+        {message.message_type === "video" && mediaUrl && (
+          <div className="mt-2 pt-2">
+            <video
+              src={mediaUrl}
+              controls
+              style={{ maxWidth: "70%", borderRadius: "10px" }}
+            />
+            {message.filename && <h6 className="mt-2">{message.filename}</h6>}
+            {message?.caption}
+          </div>
+        )}
+
+        {/*  // Display the media if the message type is media */}
+        {message.message_type === "audio" && mediaUrl && (
+          <div className="mt-2">
+            <audio controls>
+              <source src={mediaUrl} type="audio/mpeg" />
+            </audio>
+          </div>
+        )}
+
+        {/* // Display the media if the message type is media */}
+        {message.message_type === "document" && mediaUrl && (
+          <div style={{ width: "35vw" }}>
+            <div className="mb-2 d-flex pt-2">
+              <div>
+                <FaRegFileImage
+                  className="me-2"
+                  style={{
+                    fontSize: "1.5rem",
+                    color: "var(--primary)",
+                  }}
+                />
+              </div>
+              <div>
+                {message.filename && (
+                  <p className="fw-bold">{message.filename}</p>
+                )}
               </div>
             </div>
-          )
-        }
-
-        {
-          // Display the media if the message type is media
-          message.message_type === "image" && mediaUrl && (
-            <div className="mt-2">
-              <img
-                src={mediaUrl}
-                alt="media"
-                style={{ maxWidth: "70%", borderRadius: "10px" }}
-              />
-              {message?.caption}
+            <div className="d-flex justify-content-end">
+              <Link
+                href={mediaUrl}
+                className="btn btn-primary mb-2 rounded"
+                target="_blank"
+                rel="noreferrer"
+              >
+                download
+              </Link>
             </div>
-          )
-        }
 
-        {
-          // Display the media if the message type is media
-          message.message_type === "sticker" && mediaUrl && (
-            <div className="mt-2">
-              <img
-                src={mediaUrl}
-                alt="media"
-                style={{ maxWidth: "70%", borderRadius: "10px" }}
-              />
-            </div>
-          )
-        }
+            <div>{message?.caption}</div>
+          </div>
+        )}
 
-        {
-          // Display the media if the message type is media
-          message.message_type === "video" && mediaUrl && (
-            <div className="mt-2 pt-2">
-              <video
-                src={mediaUrl}
-                controls
-                style={{ maxWidth: "70%", borderRadius: "10px" }}
-              />
-              {message.filename && <h6 className="mt-2">{message.filename}</h6>}
-              {message?.caption}
-            </div>
-          )
-        }
-
-        {
-          // Display the media if the message type is media
-          message.message_type === "audio" && mediaUrl && (
-            <div className="mt-2">
-              <audio controls>
-                <source src={mediaUrl} type="audio/mpeg" />
-              </audio>
-            </div>
-          )
-        }
-
-        {
-          // Display the media if the message type is media
-          message.message_type === "document" && mediaUrl && (
-            <div style={{ width: "35vw" }}>
-              <div className="mb-2 d-flex pt-2">
-                <div>
-                  <FaRegFileImage
-                    className="me-2"
-                    style={{
-                      fontSize: "1.5rem",
-                      color: "var(--primary)",
-                    }}
-                  />
-                </div>
-                <div>
-                  {message.filename && (
-                    <p className="fw-bold">{message.filename}</p>
-                  )}
-                </div>
-              </div>
-              <div className="d-flex justify-content-end">
-                <Link
-                  href={mediaUrl}
-                  className="btn btn-primary mb-2 rounded"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  download
-                </Link>
-              </div>
-
-              <div>{message?.caption}</div>
-            </div>
-          )
-        }
-
+        {/* // Displays the timestamp and whether sent or not */}
         <div className="d-flex justify-content-end">
           <small
             style={{
